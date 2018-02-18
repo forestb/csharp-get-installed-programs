ffa38ae6e67c3e359a3cf5e1e821496433ae7293
Forest Balk <forest.balk@gmail.com>
2018-02-18 09:19:37 -0500

    Initial commit

---
+++ WindowsProgramFinder/Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                    }
+                }
+            }
+        }
+    }
+}
